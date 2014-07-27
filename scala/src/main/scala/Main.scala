ccebd8b92505603f7debc0c587c7b71123d481fd
Steven Heidel <steven@livingskyweb.ca>
2014-07-26 18:47:32 -0600

    Added initial timings

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -2,6 +2,15 @@ object Main extends App {
   val n = args(0).toInt
 
   val result = PSuccessDWaveTest(ZeroHamiltonian(n).sparseMatrix, RandomIsing(n), 3, 1000)
-  
+
   println(result)
 }
+
+/*
+15 -    0:41.76 - 1555244
+16 -    1:57.60 - 1655864
+17 -    5:06.52 - 1907628
+18 -   15:29.75 - 2291812
+19 -   51:46.16 - 3732264
+20 - 2:53:02    - 7681388
+*/
